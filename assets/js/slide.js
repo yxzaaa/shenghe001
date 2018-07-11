026a3c622c53b27bafe1f98b967e25a0f0421729
rzp <15275295437@163.com>
2018-07-11 17:08:27 +0800

    1

---
+++ assets/js/slide.js
@@ -0,0 +1,100 @@
+//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+
+$(document).ready(function(){
+	
+	
+	/*****************
+	 BUILD THE SLIDER
+	*****************/
+	//set width to be 'x' times the number of slides
+	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+	
+    //next slide 	
+	$('#next').click(function(){
+		slideRight();
+	});
+	
+	//previous slide
+	$('#previous').click(function(){
+		slideLeft();
+	});
+	
+	
+	
+	/*************************
+	 //*> OPTIONAL SETTINGS
+	************************/
+	//automatic slider
+	var autoSlider = setInterval(slideRight, 3000);
+	
+	//for each slide 
+	$.each($('#slider-wrap ul li'), function() { 
+	   //set its color
+	   var c = $(this).attr("data-color");
+	   $(this).css("background",c);
+	   
+	   //create a pagination
+	   var li = document.createElement('li');
+	   $('#pagination-wrap ul').append(li);	   
+	});
+	
+	//pagination
+	pagination();
+	
+	//hide/show controls/btns when hover
+	//pause automatic slide when hover
+	$('#slider-wrap').hover(
+	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
+	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	);
+	
+	
+
+});//DOCUMENT READY
+	
+$(window).resize(function(){
+	$('#slider-wrap ul#slider').width($('#slider-wrap').width()*totalSlides);
+})
+
+/***********
+ SLIDE LEFT
+************/
+function slideLeft(){
+	pos--;
+	if(pos==-1){ pos = totalSlides-1; }
+	$('#slider-wrap ul#slider').css('left', -($('#slider-wrap').width()*pos)); 	
+	
+	//*> optional
+	pagination();
+}
+
+
+/************
+ SLIDE RIGHT
+*************/
+function slideRight(){
+	pos++;
+	if(pos==totalSlides){ pos = 0; }
+	$('#slider-wrap ul#slider').css('left', -($('#slider-wrap').width()*pos)); 
+	
+	//*> optional 
+	pagination();
+}
+
+
+
+	
+/************************
+ //*> OPTIONAL SETTINGS
+************************/
+
+function pagination(){
+	$('#pagination-wrap ul li').removeClass('active');
+	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
+}
