b29ac84acf7ba37f25c3fb2cc55d4466f7b5f8f8
rzp <15275295437@163.com>
2018-07-11 15:29:23 +0800

    1

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+$('.icon-toggle').click(function(){
+    if($('.com-nav').hasClass('show')){
+        $('.com-nav').removeClass('show');
+    }else{
+        $('.com-nav').addClass('show');
+    }
+})
