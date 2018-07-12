ace16d3f237fa9383f06083d69512433bcbd9e94
rzp <15275295437@163.com>
2018-07-12 13:36:45 +0800

    1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,24 @@
-$('.icon-toggle').click(function(){
-    if($('.com-nav').hasClass('show')){
-        $('.com-nav').removeClass('show');
+$(document).ready(function(){
+    $('.icon-toggle').click(function(){
+        if($('.com-nav').hasClass('show')){
+            $('.com-nav').removeClass('show');
+        }else{
+            $('.com-nav').addClass('show');
+        }
+    })
+    $('[data-ac=active]').addClass('active');
+})
+$(window).scroll(function(){
+    var HEIGHT = $('.com-head-logo').height() + 2*parseInt($('.com-head-logo').css('padding'));
+    if($(window).scrollTop() >= 5){
+        $('.nav-box').addClass('nav-fixed');
+        $('.nav-box').css('transform','translateY(-'+HEIGHT+'px)');
+        $('.com-head-name').height(42);
+        $('.page-box').css('margin-top',$('.nav-box').height());
     }else{
-        $('.com-nav').addClass('show');
+        $('.nav-box').removeClass('nav-fixed');
+        $('.nav-box').css('transform','translateY(0px)');
+        $('.com-head-name').height(0);
+        $('.page-box').css('margin-top',0);
     }
 })
