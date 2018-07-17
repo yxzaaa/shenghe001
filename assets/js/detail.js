1e3d6aa1a345f47b4543f0305f50a53a063b4dcd
yxzaaa <2280517552@qq.com>
2018-07-17 17:45:57 +0800

    1

---
+++ assets/js/detail.js
@@ -0,0 +1,43 @@
+var newDetails = [
+    {
+        title:'',
+        content:''
+    }
+];
+var caseDetails = [
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case1.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case2.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case3.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case4.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case5.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case6.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case7.jpg'
+    },
+    {
+        title:'重庆奥林匹克体育中心',
+        img:'./assets/img/cases/case8.jpg'
+    },
+];
+
+var query = window.location.search;
+console.log(query);
